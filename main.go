41f7f03c2b7471f00616e5cffbe1412c474de570
Ionut Ilie <ionut.ilie@metrosystems.net>
2018-05-30 19:05:53 +0300

    exposing soe

--- main.go
+++ main.go
@@ -1,39 +1,104 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
+	"fmt"
 	"io"
-	"log"
+	"io/ioutil"
 	"net/http"
+	"regexp"
 
 	"git.metrosystems.net/reliability-engineering/traffic-monkey/internal"
+	log "git.metrosystems.net/reliability-engineering/traffic-monkey/log"
 )
 
 func healthHandler(wr http.ResponseWriter, req *http.Request) {
 	wr.WriteHeader(http.StatusOK)
 	wr.Header().Set("Content-Type", "application/json")
+	log.LogWithFields.Debug(req.URL.Path)
 	io.WriteString(wr, `{"Status": OK}`)
 }
 
 func prometheusHandler(wr http.ResponseWriter, req *http.Request) {
+	log.LogWithFields.Debug(req.URL.Path)
 	wr.WriteHeader(http.StatusOK)
 }
 
+func reports(wr http.ResponseWriter, req *http.Request) {
+	log.LogWithFields.Debug(req.URL.RawPath)
+	log.LogWithFields.Debug(req.URL.RawQuery)
+
+	if file := req.URL.Query().Get("file"); file != "" {
+		fmt.Println(file)
+
+		if match, _ := regexp.MatchString("^[a-z-0-9]+.json", file); match == true {
+			fileData, err := ioutil.ReadFile("data/" + file)
+			if err != nil {
+				log.LogWithFields.Error(err.Error())
+				return
+			}
+
+			// unamrshal and marshal again because of a stupid witespace somewere
+			var dat map[string]interface{}
+			if err := json.Unmarshal(fileData, &dat); err != nil {
+				panic(err)
+			}
+
+			data, err := json.Marshal(dat)
+			if err != nil {
+				log.LogWithFields.Error(err.Error())
+				return
+			}
+
+			wr.Header().Set("Content-Type", "application/json")
+			wr.Write(data)
+			return
+		}
+	}
+
+	files, err := ioutil.ReadDir("data")
+	if err != nil {
+		log.LogWithFields.Error(err.Error())
+		return
+	}
+
+	dataInterface := []interface{}{}
+
+	for _, file := range files {
+		dataInterface = append(dataInterface, map[string]string{"file": file.Name(), "time": file.ModTime().String()})
+	}
+
+	data, err := json.Marshal(dataInterface)
+	if err != nil {
+		log.LogWithFields.Error(err.Error())
+		return
+	}
+	wr.Header().Set("Content-Type", "application/json")
+	wr.Write(data)
+	// wr.WriteHeader(http.StatusOK)
+}
+
 func main() {
-	var addr = flag.String("addr", ":9090", "The addr of the application.")
+	var addr = flag.String("addr", ":8080", "The addr of the application.")
 	flag.Parse()
 
 	mux := http.NewServeMux()
 
 	mux.Handle("/data/", http.StripPrefix("/data/", http.FileServer(http.Dir("data"))))
+	mux.Handle("/ui/", http.StripPrefix("/ui/", http.FileServer(http.Dir("ui"))))
 
 	mux.HandleFunc("/probe", internal.URLStress)
+
+	// mux.HandleFunc("/api/v1/reports", reports)
+	mux.HandleFunc("/api/v1/reports", reports)
+
 	mux.HandleFunc("/.well-known/ready", healthHandler)
 	mux.HandleFunc("/.well-known/live", healthHandler)
 	mux.HandleFunc("/.well-known/metrics", prometheusHandler)
 
-	log.Println("Starting proxy server on", *addr)
+	log.LogWithFields.Infof("Starting proxy server on: %v", *addr)
 	if err := http.ListenAndServe(*addr, mux); err != nil {
-		log.Fatal("ListenAndServe:", err)
+		log.LogWithFields.Fatalf("ListenAndServe: %v", err.Error())
 	}
 }
