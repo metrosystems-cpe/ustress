05b111b8717cc833f34b6d5718fbc51f4a7ddeaa
stefanm8 <stefanmihartescu@gmail.com>
2019-02-06 14:38:48 +0200

    npm run build

---
+++ web/ui/build/precache-manifest.9819f1aaa51b0a229265dbae0860436a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "39dd75260b8003cafa9f",
+    "url": "/static/css/main.9b2aeb77.chunk.css"
+  },
+  {
+    "revision": "39dd75260b8003cafa9f",
+    "url": "/static/js/main.39dd7526.chunk.js"
+  },
+  {
+    "revision": "96096c9e87a82f62e4ab",
+    "url": "/static/js/1.96096c9e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "af853a72cd027c76cb85f479346efcb4",
+    "url": "/index.html"
+  }
+];
