d3136ee3aef1694d217c2917c22107480e931bbb
Stefan Mihartescu <stefan.mihartescu@metrosystems.net>
2019-02-06 14:38:48 +0200

    npm run build

--- web/ui/build/service-worker.js
+++ web/ui/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.581adb7468bc232a958e026b6b24e3c6.js"
+  "/precache-manifest.9819f1aaa51b0a229265dbae0860436a.js"
 );
 
 workbox.clientsClaim();
