b4acb71350a3d1d01c131ea1228bd0a207e47942
Stefan Mihartescu <stefan.mihartescu@metrosystems.net>
2019-02-15 11:40:33 +0200

    window.location.host

--- web/ui/src/index.js
+++ web/ui/src/index.js
@@ -4,7 +4,7 @@ import './index.scss';
 import App from './App';
 import WebSocketService from './components/utils/wsservice';
 
-export const WS = new WebSocketService("ws://localhost:8080/ustress/api/v1/ws") 
+export const WS = new WebSocketService(`ws://${window.location.host}/ustress/api/v1/ws`) 
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
